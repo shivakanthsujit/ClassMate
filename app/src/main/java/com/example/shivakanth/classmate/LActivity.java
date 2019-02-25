6ea0de359f6968c3003de095f59319ebe0d55972
Shiva Kanth <shivakanth.sujit@gmail.com>
2019-02-25 14:32:35 +0530

    Added Firebase

---
+++ app/src/main/java/com/example/shivakanth/classmate/LActivity.java
@@ -0,0 +1,13 @@
+package com.example.shivakanth.classmate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_l);
+    }
+}
--- app/src/main/java/com/example/shivakanth/classmate/LoginActivity.java
+++ app/src/main/java/com/example/shivakanth/classmate/LoginActivity.java
@@ -1,13 +1,30 @@
 package com.example.shivakanth.classmate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button login, vC;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        login = findViewById(R.id.lButton);
+        vC = findViewById(R.id.viewC);
+
+        vC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(LoginActivity.this, ClassActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
